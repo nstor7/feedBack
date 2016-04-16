003c5768e73bedbfd0ba5640ec9b02153ff71291
Néstor Ibarra <nstor7@hotmail.com>
2016-04-16 09:28:42 -0500

    cambio de servidor

--- app/servidor.js
+++ app/servidor.js
@@ -1,9 +1,12 @@
-console.log('Iniciando el Servidor de la feedback');
-
+console.log('Arrancando servidor de FeedBack');
 var express = require('express');
 var app = express();
+var parcero = require('body-parser');
+app.use(parcero.urlencoded({extended:true}));
+var servidor;
+
 servidor = app.listen(8080, function(){
-    console.log('Arrancó con Éxito el Servidor de la feedback');
+    console.log('Servidor Arrancado');
 });
 
-app.use('/', express.static('cliente'));
+app.use(express.static('cliente'));
