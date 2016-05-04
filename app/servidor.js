3bd922db114ab59cf69a599b4c7ff287e6ca103c
Néstor Ibarra <nstor7@hotmail.com>
2016-05-04 08:15:18 -0500

    no parser

--- app/servidor.js
+++ app/servidor.js
@@ -1,8 +1,6 @@
 console.log('Arrancando servidor de FeedBack');
 var express = require('express');
 var app = express();
-var parcero = require('body-parser');
-app.use(parcero.urlencoded({extended:true}));
 
 
 var servidor = app.listen(8080, function(){
