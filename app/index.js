0652d9a4ddff29717725cfd71469ce1aee217bb1
Néstor Ibarra <nstor7@hotmail.com>
2016-05-04 13:36:58 -0500

    cambio de puerto

--- app/index.js
+++ app/index.js
@@ -4,4 +4,4 @@ var app = express()
 
 app.use(express.static('app/cliente'))
 
-app.listen(8080, () => console.log('servidor iniciado en el puerto 8080'))
+app.listen(8000, () => console.log('servidor iniciado en el puerto 8000'))
